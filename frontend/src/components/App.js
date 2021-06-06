36be9b5f1794d787b21420c818a37efe26a5f509
Midaysa <mida.palacios@gmail.com>
2021-06-06 15:54:39 -0400

    Add Frontend Authentication

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
 import Header from './layout/Header';
+import Login from './accounts/Login';
+import Register from './accounts/Register';
 
 class App extends Component {
   render() {
-    return <Header />
+    return (
+    	<Router>
+    		<Header />
+    		<Switch>
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+    		</Switch>
+    	</Router>
+    );
   }
 }
 
